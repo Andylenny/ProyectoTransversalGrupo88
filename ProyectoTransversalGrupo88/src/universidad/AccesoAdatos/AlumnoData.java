178d398e7a1930c13efe74f79b841dc10a060c30
ezequielsch <141652683+ezequielsch@users.noreply.github.com>
2023-09-12 11:58:52 -0300

    agrege alumno

--- ProyectoTransversalGrupo88/src/universidad/AccesoAdatos/AlumnoData.java
+++ ProyectoTransversalGrupo88/src/universidad/AccesoAdatos/AlumnoData.java
@@ -10,5 +10,37 @@ package universidad.AccesoAdatos;
  * @author Lourdes
  */
 public class AlumnoData {
-    
+    String nombre;
+    int edad;
+    int dni;
+
+    public AlumnoData(String nombre, int edad, int dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
 }
