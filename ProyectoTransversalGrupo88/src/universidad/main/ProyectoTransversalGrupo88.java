a4c4a64d9bbd2165d9670dd0b51bba1554067c69
Lourrios <141644661+Lourrios@users.noreply.github.com>
2023-09-12 11:21:40 -0300

    Add files via upload

---
+++ ProyectoTransversalGrupo88/src/universidad/main/ProyectoTransversalGrupo88.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidad.main;
+
+/**
+ *
+ * @author Lourdes
+ */
+public class ProyectoTransversalGrupo88 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
